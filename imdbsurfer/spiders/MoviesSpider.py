979711af13011cb688c546bb2f38e16a3e45b313
Eleonor Vinicius <viniciusmayer@gmail.com>
2017-06-24 11:26:15 -0300

    Data extraction from Imdb advanced search done

--- imdbsurfer/spiders/MoviesSpider.py
+++ imdbsurfer/spiders/MoviesSpider.py
@@ -3,39 +3,70 @@ import scrapy
 from imdbsurfer import items
 
 def votes(value):
-    return cls(value, 1)
+    return extractAndClean(value, 1)
 
 def index(value):
-    return cls(value).replace('.', '')
+    return extractAndClean(value).replace('.', '')
 
 def year(value):
-    return cls(value).replace('(', '').replace(')', '')
+    return extractAndClean(value).replace('(', '').replace(')', '')
 
 def minutes(value):
-    return value[0].split()[0].rstrip().lstrip().strip('\n').strip('\t').strip('\r')
+    return clean(value[0].split()[0])
 
 def genres(value):
     _value = []
     for i in value[0].split(","):
-        _value.append(i.rstrip().lstrip().strip('\n').strip('\t').strip('\r'))
+        _value.append(clean(i))
     return _value
 
 def link(value):
-    _value = value[0]
-    return _value[:_value.find('?') - 1]
+    return value[0][:value[0].find('?') - 1]
 
-def cls(value, pos=0):
-    if len(value) > 0:
-        return value[pos].rstrip().lstrip().strip('\n').strip('\t').strip('\r')
-    else:
-        return None
-    
+def extractAndClean(value, pos=0):
+    if value is not None and len(value) > 0:
+        return clean(value[pos])
+    return None
+
+def clean(value):
+    if value is not None:
+        return value.rstrip().lstrip().strip('\n').strip('\t').strip('\r')
+    return None
 
 class MoviesSpider(scrapy.Spider):
+    genres = ['action',
+        'adventure',
+        'animation',
+        'biography',
+        'comedy',
+        'crime',
+        'documentary',
+        'drama',
+        'family',
+        'fantasy',
+        'film_noir',
+        'game_show',
+        'history',
+        'horror',
+        'music',
+        'musical',
+        'mystery',
+        'news',
+        'reality_tv',
+        'romance',
+        'sci_fi',
+        'sport',
+        'talk_show',
+        'thriller',
+        'war',
+        'western']
+    
     name = "movies"
-    start_urls = [
-        'http://www.imdb.com/search/title?count=100&genres=adventure,biography&num_votes=10000,&title_type=feature&sort=user_rating,desc',
-    ]
+    start_urls = []
+    
+    for i in genres:
+        url = 'http://www.imdb.com/search/title?genres={0}&num_votes=10000,&title_type=feature&sort=user_rating,desc' 
+        start_urls.append(url.format(i)) 
 
     def parse(self, response):
         for i in response.css('div[class="lister-item-content"]'):
@@ -43,11 +74,11 @@ class MoviesSpider(scrapy.Spider):
             item['index'] = index(i.css('h3[class="lister-item-header"]').css('span[class="lister-item-index unbold text-primary"]::text').extract())
             item['year'] = year(i.css('h3[class="lister-item-header"]').css('span[class="lister-item-year text-muted unbold"]::text').extract())
             item['link'] = link(i.css('h3[class="lister-item-header"]').css('a::attr(href)').extract())
-            item['name'] = cls(i.css('h3[class="lister-item-header"]').css('a::text').extract())
+            item['name'] = extractAndClean(i.css('h3[class="lister-item-header"]').css('a::text').extract())
             item['genres'] = genres(i.css('p[class="text-muted "]').css('span[class="genre"]::text').extract())
             item['minutes'] = minutes(i.css('p[class="text-muted "]').css('span[class="runtime"]::text').extract())
-            item['rate'] = cls(i.css('div[class="ratings-bar"]').css('div[class="inline-block ratings-imdb-rating"]').css('strong::text').extract())
-            item['metascore'] = cls(i.css('div[class="ratings-bar"]').css('div[class="inline-block ratings-metascore"]').css('span[class="metascore  favorable"]::text').extract())
-            item['director'] = cls(i.css('p[class=""]').css('a::text').extract())
+            item['rate'] = extractAndClean(i.css('div[class="ratings-bar"]').css('div[class="inline-block ratings-imdb-rating"]').css('strong::text').extract())
+            item['metascore'] = extractAndClean(i.css('div[class="ratings-bar"]').css('div[class="inline-block ratings-metascore"]').css('span[class="metascore  favorable"]::text').extract())
+            item['director'] = extractAndClean(i.css('p[class=""]').css('a::text').extract())
             item['votes'] = votes(i.css('p[class="sort-num_votes-visible"]').css('span::text').extract())            
             yield item

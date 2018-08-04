7c315ed40910ed5938ac26257702f0e162ccd321
Vinicius Mayer <viniciusmayer@gmail.com>
2018-08-04 11:01:37 -0300

    Updating script and including year as an criteria of the final index

--- SQLs/get_movie_index.sql
+++ SQLs/get_movie_index.sql
@@ -22,6 +22,8 @@ DECLARE _index decimal;
 	max_votes decimal;
 	min_rate decimal;
 	max_rate decimal;
+	min_year decimal;
+	max_year decimal;
 BEGIN
 	select min(rate) into min_rate from imdbsurfer_movie;
 	select max(rate) into max_rate from imdbsurfer_movie;
@@ -29,29 +31,35 @@ BEGIN
 	select max(metascore) into max_metascore from imdbsurfer_movie;
 	select min(votes) into min_votes from imdbsurfer_movie;
 	select max(votes) into max_votes from imdbsurfer_movie;
+	select min(year) into min_year from imdbsurfer_movie;
+	select max(year) into max_year from imdbsurfer_movie;
 	select min(index) into min_index from imdbsurfer_moviegenre;
 	select max(index) into max_index from imdbsurfer_moviegenre;
 
 	select case
 		when m.metascore is not null and mg.index is not null
-			then (m.rate * 2.5
-				  + (m.metascore / 10) * 2.5
-				  + rescale(m.votes, min_votes, max_votes, 0, 10) * 2.5
-				  + (10 / rescale(mg.index, min_index, max_index, 1, 10)) * 2.5
+			then (m.rate * 2
+				  + (m.metascore / 10) * 2
+				  + rescale(m.votes, min_votes, max_votes, 0, 10) * 2
+				  + (10 / rescale(mg.index, min_index, max_index, 1, 10)) * 2
+				  + rescale(m.year, min_year, max_year, 1, 10) * 2
 				 ) / 10
 		when m.metascore is not null and mg.index is null
-			then (m.rate * 3
-				  + (m.metascore / 10) * 3
-				  + rescale(m.votes, min_votes, max_votes, 0, 10) * 3
-				 ) / 10
+			then (m.rate * 2
+				  + (m.metascore / 10) * 2
+				  + rescale(m.votes, min_votes, max_votes, 0, 10) * 2
+				  + rescale(m.year, min_year, max_year, 1, 10) * 2
+				 ) / 9
 		when m.metascore is null and mg.index is not null
-			then (m.rate * 3
-				  + rescale(m.votes, min_votes, max_votes, 0, 10) * 3
-				  + (10 / rescale(mg.index, min_index, max_index, 1, 10)) * 3
-				 ) / 10
-		else (m.rate * 4
-			  + rescale(m.votes, min_votes, max_votes, 0, 10) * 4
-			 ) / 10
+			then (m.rate * 2
+				  + rescale(m.votes, min_votes, max_votes, 0, 10) * 2
+				  + (10 / rescale(mg.index, min_index, max_index, 1, 10)) * 2
+				  + rescale(m.year, min_year, max_year, 1, 10) * 2
+				 ) / 9
+		else (m.rate * 2
+			  + rescale(m.votes, min_votes, max_votes, 0, 10) * 2
+			  + rescale(m.year, min_year, max_year, 1, 10) * 2
+			 ) / 8
 		end as cindex into _index
 	from imdbsurfer_movie m
 		inner join imdbsurfer_moviegenre mg on mg.movie_id=m.id and m.id = _movie_id
--- imdbsurfer.sh
+++ imdbsurfer.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
-date
+echo "ScrapyCrawl begin"
+inicio=$(date +"%s")
+docker start postgres
 cd /home/eleonorvinicius/Projects/imdbsurfer
 source bin/activate
-scrapy crawl movies > movies.log 2>&1
+scrapy crawl movies
 python3 Main.py
+fim=$(date +"%s")
+dif=$(($fim-$inicio))
+horas=$(($dif/3600))
+minutos=$((($dif%3600)/60))
+segundos=$(($dif%60))
+echo "ScrapyCrawl end: ${horas}h:${minutos}m:${segundos}s"
 date
